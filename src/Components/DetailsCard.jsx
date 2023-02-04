6192a68ef68a934cd4eb31e510dc66b9ed1f0fe3
Kuldeep <c0dysharma01@gmail.com>
2023-02-04 14:15:25 +0530

    flex to grid

--- src/Components/DetailsCard.jsx
+++ src/Components/DetailsCard.jsx
@@ -1,23 +1,25 @@
 const DetailsCard = ({ ipAddress, cityCountry, timeZone, isp }) => (
   <section className="flex justify-center mx-8 font-medium">
-    <div className="bg-white max-w-xl w-full flex flex-col gap-5 items-center rounded-xl shadow-md py-10 px-8 md:flex-row md:max-w-7xl md:divide-x-2 md:gap-16 md:justify-evenly ">
+    <div className="bg-white w-full max-w-7xl grid grid-cols-1 gap-y-5 rounded-xl shadow-md py-10 px-8 md:grid-cols-2 lg:grid-cols-4 lg:gap-x-28 lg:divide-x-2">
       <div className="text-center md:text-left">
-        <p className="uppercase text-gray-400 text-xs mb-1">Ip Address</p>
+        <p className="uppercase text-gray-400 text-xs mb-1 lg:mb-2">
+          Ip Address
+        </p>
         <p className="text-xl md:text-xl lg:text-2xl">{ipAddress}</p>
       </div>
 
-      <div className="text-center md:pl-7 md:text-left">
-        <p className="uppercase text-gray-400 text-xs mb-1">Location</p>
+      <div className="text-center lg:pl-7 md:text-left">
+        <p className="uppercase text-gray-400 text-xs mb-1 lg:mb-2">Location</p>
         <p className="text-xl md:text-xl lg:text-2xl">{cityCountry}</p>
       </div>
 
-      <div className="text-center md:pl-7 md:text-left">
-        <p className="uppercase text-gray-400 text-xs mb-1">Timezone</p>
+      <div className="text-center lg:pl-7 md:text-left">
+        <p className="uppercase text-gray-400 text-xs mb-1 lg:mb-2">Timezone</p>
         <p className="text-xl md:text-xl lg:text-2xl">{timeZone}</p>
       </div>
 
-      <div className="text-center md:pl-7 md:text-left">
-        <p className="uppercase text-gray-400 text-xs mb-1">ISP</p>
+      <div className="text-center lg:pl-7 md:text-left">
+        <p className="uppercase text-gray-400 text-xs mb-1 lg:mb-2">ISP</p>
         <p className="text-xl md:text-xl lg:text-2xl">{isp}</p>
       </div>
     </div>
